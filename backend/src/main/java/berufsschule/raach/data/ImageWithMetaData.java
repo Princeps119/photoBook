6806b83805e3ed8e7a040cbd96a068e54247c059
Robin Mössinger <Robinmoessinger@web.de>
2026-02-05 21:02:51 +0100

    javadoc

--- backend/src/main/java/berufsschule/raach/data/ImageWithMetaData.java
+++ backend/src/main/java/berufsschule/raach/data/ImageWithMetaData.java
@@ -1,4 +1,29 @@
 package berufsschule.raach.data;
 
-public record ImageWithMetaData(String hexString, String filename, org.bson.Document metadata, byte[] byteArray) {}
+import org.bson.Document;
+
+/**
+ *
+ * @param hexStringId the MongoDB/GridFSFile id of the image
+ * @param filename name of the image
+ * @param metadata must contain a tag key with either Private or Public value
+ * @param byteArray the image itself
+ *
+ * Mock JSON
+ * <p>
+        {
+        "hexString": "507f1f77bcf86cd799439011",
+        "filename": "profile_picture.png",
+        "metadata": {
+        "userMail": "alice@example.com",
+        "contentType": "image/png",
+        "tag": "Public",
+        "uploadDate": "2026-02-05T17:40:00Z"
+        },
+        "byteArray": "iVBORw0KGgoAAAANSUhEUgAAAAUA"
+        }
+ </p>
+
+ */
+public record ImageWithMetaData(String hexStringId, String filename, Document metadata, byte[] byteArray) {}
 
