69376c5d70cf1a9a133297e9302eabee562af9e8
Robin Mössinger <robin.moessinger@berner.eu>
2026-02-06 08:43:39 +0100

    better JSON mock formatting in javadoc

--- backend/src/main/java/berufsschule/raach/data/ImageWithMetaData.java
+++ backend/src/main/java/berufsschule/raach/data/ImageWithMetaData.java
@@ -9,18 +9,18 @@ import org.bson.Document;
  * @param filename name of the image
  * @param metadata must contain a tag key with either Private or Public value
  * @param byteArray the image itself
- *
+ *<p>
  * Mock JSON
- * <p>
-        {
-                "hexString": "507f1f77bcf86cd799439011",
-                "filename": "profile_picture.png",
-                "metadata": {
+ *<p>
+        {<p>
+                "hexString": "507f1f77bcf86cd799439011",<p>
+                "filename": "profile_picture.png",<p>
+                "metadata": {<p>
                         "userMail": "alice@example.com", -optional-
                         "contentType": "image/png", -optional-
                         "tag": "Public",
                         "uploadDate": "2026-02-05T17:40:00Z" -optional-
-                        },
+                        },<p>
                 "byteArray": "iVBORw0KGgoAAAANSUhEUgAAAAUA"
         }
  </p>
